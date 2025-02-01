edc74b789395b26c3b829f08f59eb77f96ff390c
Feelgoodmann <tanapat.ch@kkumail.com>
2025-02-02 02:27:16 +0700

    With List & Api

--- app/src/main/java/com/example/linecoinproject/Main.kt
+++ app/src/main/java/com/example/linecoinproject/Main.kt
@@ -1,38 +1,107 @@
 package com.example.linecoinproject
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import com.example.linecoinproject.api.SimpleApi
 import com.example.linecoinproject.databinding.MainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
+
 
 class Main : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: MainBinding
+    private lateinit var simpleApi: SimpleApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolBar)
-        setSupportActionBar(binding.toolBar)
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-//        binding.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null)
-//                .setAnchorView(R.id.fab).show()
-//        }
+        val toolbar = findViewById<Toolbar>(R.id.toolBar)
+        setSupportActionBar(toolbar)
+        val listView = findViewById<ListView>(R.id.main_listview)
+
+        val apiUrl = "https://api.coinranking.com/v2/coins"
+        val response = StringBuilder()
+        try {
+            val url: URL = URI.create(apiUrl).toURL()
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+            //Request method: GET
+            connection.requestMethod = "GET"
+
+            // Response code
+            val responseCode: Int = connection.responseCode
+            println("Response Code: $responseCode")
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read and print the response data
+                val reader: BufferedReader = BufferedReader(InputStreamReader(connection.inputStream))
+                var line: String?
+
+
+                while (reader.readLine().also { line = it } != null) {
+                    response.append(line)
+                }
+
+                reader.close()
+
+                println("Response Data: $response")
+            } else {
+                println("Error: Unable to fetch data from the API")
+            }
+
+            // Close the connection
+            connection.disconnect()
+        } catch (e: Exception) {
+            e.printStackTrace()
+
+        }
+        println("TWO STEP AHEAD ${response}")
+        listView.adapter = EachCoin(this)
+
+
+    }
+    private class EachCoin(context: Context): BaseAdapter(){
+
+        private val mContext: Context
+            init {
+                mContext = context
+            }
+
+        override fun getCount(): Int {
+            return 10
+        }
+
+        override fun getItem(p0: Int): Any {
+            return "TEST STRING"
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        @SuppressLint("ViewHolder")
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val coin = layoutInflater.inflate(R.layout.coin, p2, false)
+            return coin
+        }
     }
 }
---
+++ app/src/main/java/com/example/linecoinproject/components/Coin.kt
@@ -0,0 +1,11 @@
+package com.example.linecoinproject.components
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class Coin<T>{
+
+
+}
